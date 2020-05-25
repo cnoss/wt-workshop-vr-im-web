55c942c2781122d1bba103ade236d7864b17dfc8
Pascal Schorde <pascal.schorde@gmail.com>
2020-05-25 22:57:51 +0200

    add server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+const index = 'index.html';
+
+const server = http.createServer((req, res) => {
+    if (req.url == '/') {
+        req.url = '/' + index;
+    }
+    fs.readFile(__dirname + req.url, (err, data) => {
+        if (err) {
+            res.writeHead(404);
+            res.end(JSON.stringify(err));
+            return;
+        }
+        res.statusCode = 200;
+        res.end(data);
+    })
+    
+});
+
+server.listen(port, hostname, () => {
+    console.log('Server running');
+});
